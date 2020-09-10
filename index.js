4fa1cec1c84bae3b4a5ca16c5e8183dc3f96439f
Natasha-Moon <natashamoon1304@gmail.com>
2020-09-10 14:04:28 +1000

    add in JavaScript file

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create a JavaScript function called “validateTaskForm” that verifies that the inputs inserted by the user in the task form are correct
+
+// Name -> Not Empty and longer than 8 characters
+
+// Description -> Not Empty and longer than 15 characters
+
+// AssignedTo -> Not Empty and longer than 8 characters
