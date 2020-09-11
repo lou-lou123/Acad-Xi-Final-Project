a0295cf2a23c50a10f58048011e94ac98de60bd3
Natasha-Moon <natashamoon1304@gmail.com>
2020-09-11 15:27:07 +1000

    add input into an object inside variable

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-// Create a JavaScript function called “validateTaskForm” that verifies that the inputs inserted by the user in the task form are correct
 
 const formValidate = document.querySelector(".form-validate");
+
+// variables for form fields
 const formValidateAssignee = document.querySelector("#assignee");
 const formValidateTaskDescription = document.querySelector("#taskDescription");
 const formValidateTaskName = document.querySelector("#taskName");
-let taskNameInput = 0;
-let completedForm = {name:""};
 
+// variables for object
+let taskNameInput = 0;
+let completedForm = {name:'', description:'', assignee: ''};
+
+// event listener for click on submit button
 formValidate.addEventListener("submit", (event) => {
   event.preventDefault();
+
 // AssignedTo -> Not Empty and longer than 8 characters
   if (formValidateAssignee.value.length >= 8) {
     formValidateAssignee.classList.add("is-valid");
     formValidateAssignee.classList.remove("is-invalid");
+// store input value into variable - Assignee -> String
+    completedForm.assignee = formValidateAssignee.value;
+// error message for incorrect input
   } else {
     formValidateAssignee.classList.add("is-invalid");
     formValidateAssignee.classList.remove("is-valid");
   }
+
 // Description -> Not Empty and longer than 15 characters
   if (formValidateTaskDescription.value.length >= 15) {
     formValidateTaskDescription.classList.add("is-valid");
     formValidateTaskDescription.classList.remove("is-invalid");
+// store input value into variable - Description -> String
+    completedForm.description = formValidateTaskDescription.value;
+// error message for incorrect input
   } else {
     formValidateTaskDescription.classList.add("is-invalid");
     formValidateTaskDescription.classList.remove("is-valid");
   }
+
 // Name -> Not Empty and longer than 8 characters
   if (formValidateTaskName.value.length >= 8) {
     formValidateTaskName.classList.add("is-valid");
     formValidateTaskName.classList.remove("is-invalid");
 // store input value into variable - Name -> String
-    taskNameInput = formValidateTaskName.value;
+    completedForm.name = formValidateTaskName.value;
+// error message for incorrect input
   } else {
     formValidateTaskName.classList.add("is-invalid");
     formValidateTaskName.classList.remove("is-valid");
   }
+
     console.log(completedForm);
 });
-
-
-
-
-// Store input value into a variable
-// ID -> Int
-// Description -> String
